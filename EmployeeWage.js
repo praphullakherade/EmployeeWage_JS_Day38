761fe717d42604fa2de906dc78db31b3874b8a1e
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-20 18:48:03 +0530

    Employee Wages for Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,21 +4,28 @@ const isFullTime = 2;
 const partTimeHour = 4;
 const fullTimeHours = 8;
 const wagePerHour = 20;
+const numOfWorkingDays = 20;
 
 let empWorkingHours = 0;
 let empWage = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-function getWorkingHours(empCheck) {
-    switch (empCheck) {
-        case isFullTime:
-            return fullTimeHours;
+let totalEmpWage = 0;
+
+for (let day = 0; day <= numOfWorkingDays; day++){
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    switch(empCheck){
         case isPartTime:
-            return partTimeHour;
+            empWorkingHours = 4;
+            break;
+        case isFullTime:
+            empWorkingHours = 8;
+            break;
         default:
-            return 0;
+            empWorkingHours = 0;    
     }
+    empWage = empWorkingHours * wagePerHour;
+    totalEmpWage +=empWage;
+    console.log("Employee Wage is : " + empWage);
 }
-empWorkingHours = getWorkingHours(empCheck);n
-empWage = empWorkingHours * wagePerHour;
-console.log("Employee Wage is : " + empWage);
+console.log("Total Employee Wage is: " + totalEmpWage);
+
 
