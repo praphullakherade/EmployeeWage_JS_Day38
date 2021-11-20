19b060025c35dcb9259df233b0b9cd8370b2a8b6
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-20 11:09:00 +0530

    Check Employee is Present or Absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,10 @@
 console.log("Welcome to Employee Wage JS Problem");
+
+const isPresent = 0;
+
+let empCheck = Math.floor(Math.random()*10) % 2;
+if (empCheck == isPresent){
+    console.log("Employee is Present");
+} else {
+    console.log("Employee is Absent");
+}
